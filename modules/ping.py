e380e7de52e70840042ef878472c7f17f8651b7e
dimaluts2023 <dima.luts.standoff@gmail.com>
2023-09-21 09:18:28 +0200

    Update 1.0.2

--- modules/ping.py
+++ modules/ping.py
@@ -8,9 +8,9 @@ from time import time
 @Client.on_message(filters.command("ping", prefix) & filters.me)
 async def ping(_, message: Message):
     t1 = time()
-    await message.edit('Понг')
+    await message.edit('<emoji id="6255963511252322252">✔️</emoji> понг..')
     t2 = time()
-    await message.edit(f'Понг.. (0.{int((t2-t1)*1000)} ms)')
+    await message.edit(f'<emoji id="6255963511252322252">✔️</emoji> понг.. (0.{int((t2-t1)*1000)} ms)')
 
 modules_help['ping'] = {
     'ping': 'проверить скорость отклика Telegram.'
---
+++ modules/prefix.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.db import db
+from utils.scripts import restart
+from utils.misc import modules_help, prefix
+
+
+@Client.on_message(
+    filters.command(["sp", "setprefix", "setprefix_yumo"], prefix)
+    & filters.me
+)
+async def setprefix(_, message: Message):
+    if len(message.command) > 1:
+        pref = message.command[1]
+        db.set("core.main", "prefix", pref)
+        await message.edit(f"<b>Префикс [ <code>{pref}</code> ] установлен!</b>")
+        restart()
+    else:
+        await message.edit("<b>Префикс не может быть пустой!</b>")
+
+
+@Client.on_message(filters.command('prefix', prefix) & filters.me)
+async def prefix(_, message: Message):
+    await message.edit(f'Текущий префикс [ <code>{prefix}<code> ]')
+
+
+modules_help["prefix"] = {
+    "setprefix [prefix]": "установить свой префикс.",
+    "prefix": "посмотреть префикс",
+}
